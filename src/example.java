930d34bc6aac5f47ca4ed8701b005ebe71d797f5
RIEAN <riecad123@gmail.com>
2021-09-07 15:12:21 +0530

    variables added

---
+++ src/example.java
@@ -0,0 +1,15 @@
+/*
+first java program(07/09)
+* */
+ class example {
+     public static void main(String[] args)
+     {
+         int num=10;
+         System.out.println("num = "+ num);
+         num= 2* num;
+         System.out.println(num);
+
+     }
+
+}
+
